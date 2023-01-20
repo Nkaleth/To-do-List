cea8cb5ebec82ee17b7fa65119dcf44995a7e6f7
Nkaleth <nseguralu@gmail.com>
2023-01-19 23:47:11 -0500

    add remove task functionality

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 /* import _, { add } from 'lodash'; */
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Go for a walk',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Take the dog out',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Reading a book',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Coding challenge',
-    completed: true,
-    index: 3,
-  },
-];
+let tasks = [];
 
 class Task {
   constructor(description, index, completed = false) {
@@ -30,8 +9,24 @@ class Task {
     this.index = index;
     this.completed = completed;
   }
-}
 
+  static removeObWithId = (arr, id) => {
+    const ObToRemove = arr.findIndex((obj) => obj.index === id);
+    if (ObToRemove > -1) {
+      arr.splice(ObToRemove, 1);
+    }
+    return arr;
+  };
+
+  static renewIndex = (arr) => {
+    let index = 0;
+    arr.forEach((element) => {
+      element.index = index;
+      index++;
+    });
+    return arr;
+  };
+}
 const addContainer = document.querySelector('.addContainer');
 
 const todolist = document.querySelector('.list');
@@ -41,8 +36,8 @@ const LoadList = () => {
   tasks.forEach((element) => {
     string += `<li class="taskContainer">
     <button class="checkbox" type="submit"></button>
-    <span class="task">${element.description}</span>
-    <button class="dots"></button>
+    <span id="${element.index}" class="task">${element.description}</span>
+    <button id="${element.index}" class="delete"></button>
   </li>`;
   });
   todolist.innerHTML = string;
@@ -50,15 +45,29 @@ const LoadList = () => {
 
 LoadList();
 
+/* Add new tasks */
 const Addtask = (e) => {
   e.preventDefault();
   const description = addContainer.elements[0].value;
   const count = tasks.length;
   const task = new Task(description, count);
   tasks.push(task);
-  /* console.log(tasks); */
   LoadList();
   addContainer.reset();
 };
 
 addContainer.addEventListener('submit', Addtask);
+
+/* Remove task */
+
+const section = document.querySelector('.list');
+
+section.addEventListener('click', (event) => {
+  const { target } = event;
+  const NewId = target.id;
+  if (target.type === 'submit') {
+    Task.removeObWithId(tasks, NewId * 1);
+    LoadList();
+  }
+});
+
